d1321739253818458b956ee12afd24dfeddd4849
derpsol <franklyjin029@gmail.com>
2023-07-18 10:36:06 -0700

    update coding style

--- src/state/application/hooks.ts
+++ src/state/application/hooks.ts
@@ -1,24 +1,37 @@
-import { useCallback, useMemo } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useActiveWeb3React } from '../../hooks';
-import { AppDispatch, AppState } from '../index';
-import { addPopup, ApplicationModal, PopupContent, removePopup, setOpenModal } from './actions';
+import { useCallback, useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useActiveWeb3React } from "../../hooks";
+import { AppDispatch, AppState } from "../index";
+import {
+  addPopup,
+  ApplicationModal,
+  PopupContent,
+  removePopup,
+  setOpenModal,
+} from "./actions";
 
 export function useBlockNumber(): number | undefined {
   const { chainId } = useActiveWeb3React();
 
-  return useSelector((state: AppState) => state.application.blockNumber[chainId ?? -1]);
+  return useSelector(
+    (state: AppState) => state.application.blockNumber[chainId ?? -1]
+  );
 }
 
 export function useModalOpen(modal: ApplicationModal): boolean {
-  const openModal = useSelector((state: AppState) => state.application.openModal);
+  const openModal = useSelector(
+    (state: AppState) => state.application.openModal
+  );
   return openModal === modal;
 }
 
 export function useToggleModal(modal: ApplicationModal): () => void {
   const open = useModalOpen(modal);
   const dispatch = useDispatch<AppDispatch>();
-  return useCallback(() => dispatch(setOpenModal(open ? null : modal)), [dispatch, modal, open]);
+  return useCallback(
+    () => dispatch(setOpenModal(open ? null : modal)),
+    [dispatch, modal, open]
+  );
 }
 
 export function useOpenModal(modal: ApplicationModal): () => void {
@@ -67,7 +80,7 @@ export function useAddPopup(): (content: PopupContent, key?: string) => void {
     (content: PopupContent, key?: string) => {
       dispatch(addPopup({ content, key }));
     },
-    [dispatch],
+    [dispatch]
   );
 }
 
@@ -78,12 +91,12 @@ export function useRemovePopup(): (key: string) => void {
     (key: string) => {
       dispatch(removePopup({ key }));
     },
-    [dispatch],
+    [dispatch]
   );
 }
 
 // get the list of active popups
-export function useActivePopups(): AppState['application']['popupList'] {
+export function useActivePopups(): AppState["application"]["popupList"] {
   const list = useSelector((state: AppState) => state.application.popupList);
   return useMemo(() => list.filter((item) => item.show), [list]);
 }
